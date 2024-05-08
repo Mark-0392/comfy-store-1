49da27cf777a75302205d20e673ee2ec5cecf3ea
Mark-0392 <itsmxd03gmail.com>
2024-05-08 13:42:56 +0530

    'added cart items'

---
+++ src/Components/CartItemsList.jsx
@@ -0,0 +1,4 @@
+const CartItemsList = () => {
+  return <h1 className="text-3xl">hello there</h1>
+}
+export default CartItemsList
---
+++ src/Components/CartTotals.jsx
@@ -0,0 +1,4 @@
+const CartTotals = () => {
+  return <h1>Hello there</h1>
+}
+export default CartTotals
